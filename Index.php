f481b0417f8257cdf361ca3386ce6c950986793b
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2025-06-26 00:24:03 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -10,6 +10,7 @@ require_once(BASE_PATH . "/model/Musica.php");
 require_once(BASE_PATH . "/dao/MusicaDAO.php");
 
 $msgErro = "";
+$errosCampos = [];
 
 $titulo = "";
 $genero = "";
@@ -18,9 +19,7 @@ $linkImagem = "";
 $corCard = "";
 $linkMusica = "";
 
-//fazer mais verificacoes em cada um
-if(isset($_POST["titulo"]) || isset($_POST["genero"]) || isset($_POST["artista"]) || isset($_POST["linkImagem"]) || isset($_POST["corCard"]) || isset($_POST["linkMusica"]))
-{
+if (isset($_POST["titulo"])) {
     $titulo = trim($_POST["titulo"]);
     $genero = $_POST["genero"];
     $artista = trim($_POST["artista"]);
@@ -28,81 +27,53 @@ if(isset($_POST["titulo"]) || isset($_POST["genero"]) || isset($_POST["artista"]
     $corCard = $_POST["corCard"];
     $linkMusica = trim($_POST["linkMusica"]);
 
-    $erros = array();
     $musicaDao = new MusicaDAO();
 
-    //referente ao titulo
-    if(!$titulo)
-    {
-        array_push($erros,"Informe o titulo");
-    }elseif (strlen($titulo) < 3) 
-    {
-        array_push($erros,"Seu título deve ter no mínimo 3 caracteres.");
-    }elseif (strlen($titulo) > 50) 
-    {
-        array_push($erros,"Seu título deve ter no máximo 50 caracteres.");
-    }elseif ($musicaDao->buscarPorTituloRepetido($titulo) > 0)
-    {
-        array_push($erros, "O título já foi cadastrado na tabela.");  
+    if (!$titulo) {
+        $errosCampos['titulo'] = "Informe o título";
+    } elseif (strlen($titulo) < 3) {
+        $errosCampos['titulo'] = "Seu título deve ter no mínimo 3 caracteres.";
+    } elseif (strlen($titulo) > 50) {
+        $errosCampos['titulo'] = "Seu título deve ter no máximo 50 caracteres.";
+    } elseif ($musicaDao->buscarPorTituloRepetido($titulo) > 0) {
+        $errosCampos['titulo'] = "O título já foi cadastrado na tabela.";
     }
 
-    //referente ao genero, como é um select, nao vimos necessidade de colocar mais verificacoes
-    if(!$genero)
-    {
-        array_push($erros,"Informe o gênero");
+    if (!$genero) {
+        $errosCampos['genero'] = "Informe o gênero";
     }
 
-    //referente ao corCard, como é um select, resolvemos deixar somente com uma verificacao
-    if(!$corCard)
-    {
-        array_push($erros,"Informe a cor do card");
+    if (!$corCard) {
+        $errosCampos['corCard'] = "Informe a cor do card";
     }
 
-    //referente ao artista
-    if(!$artista)
-    {
-        array_push($erros,"Informe o artista");
-    }elseif (strlen($artista) < 3) 
-    {
-        array_push($erros,"O nome do artista deve ter mais de 3 caracteres");
-    }elseif(strlen($artista) > 50)
-    {
-        array_push($erros,"O nome do artista deve ter menos de 50 caracteres");
-    }
-      
-    //referente ao linkImagem
-    if(!$linkImagem)
-    {
-        array_push($erros,"Informe o link da imagem");
-    }elseif (!filter_var($linkImagem, FILTER_VALIDATE_URL))
-    {
-        array_push($erros, "Você inseriu um link inválido.");
+    if (!$artista) {
+        $errosCampos['artista'] = "Informe o artista";
+    } elseif (strlen($artista) < 3) {
+        $errosCampos['artista'] = "O nome do artista deve ter mais de 3 caracteres";
+    } elseif (strlen($artista) > 50) {
+        $errosCampos['artista'] = "O nome do artista deve ter menos de 50 caracteres";
     }
 
-    //referente ao linkMusica
-    if(!$linkMusica)
-    {
-        array_push($erros,"Informe o link da música");
-    }elseif (!filter_var($linkMusica, FILTER_VALIDATE_URL)) 
-    {
-        array_push($erros, "Você inseriu um link inválido.");
+    if (!$linkImagem) {
+        $errosCampos['linkImagem'] = "Informe o link da imagem";
+    } elseif (!filter_var($linkImagem, FILTER_VALIDATE_URL)) {
+        $errosCampos['linkImagem'] = "Você inseriu um link inválido.";
     }
 
-    //se nao cair em nenhum
-    if(count($erros) == 0)
-    {
-        $musica = new Musica($titulo,$genero,$artista,$linkImagem,$corCard,$linkMusica);
+    if (!$linkMusica) {
+        $errosCampos['linkMusica'] = "Informe o link da música";
+    } elseif (!filter_var($linkMusica, FILTER_VALIDATE_URL)) {
+        $errosCampos['linkMusica'] = "Você inseriu um link inválido.";
+    }
+
+    if (count($errosCampos) == 0) {
+        $musica = new Musica($titulo, $genero, $artista, $linkImagem, $corCard, $linkMusica);
         $musicaDao->inserirMusicas($musica);
-
-        //limpa o buffer do navegador
         header("location: Index.php");
-    }else
-    {
-        $msgErro = implode("<br>", $erros);
-        //arrumar isso para aparecer bonito no HTML, provavel arrumar com CSS
     }
-
 }
+
 
 ?>
 <!DOCTYPE html>
@@ -122,55 +93,83 @@ if(isset($_POST["titulo"]) || isset($_POST["genero"]) || isset($_POST["artista"]
         <!-- AQUI É A PARTE DO FORMULÁRIO -->
         <h1>Formulário</h1>
 
-        <form action="" method="POST">
-            <div style="margin-bottom: 10px;">
-                <label for="titulo">Título: </label>
-                <input type="text" name="titulo" id="titulo" value="<?= $titulo ?>"/>
-            </div>
+            <form action="" method="POST">
+        <div>
+            <label for="titulo">Título: </label>
+            <input type="text" name="titulo" id="titulo"
+                value="<?= $titulo ?>"
+                class="<?= isset($errosCampos['titulo']) ? 'input-erro' : '' ?>"/>
+            <?php if (isset($errosCampos['titulo'])): ?>
+                <div class="mensagem-erro"><?= $errosCampos['titulo'] ?></div>
+            <?php endif; ?>
+        </div>
 
+        <div>
+            <label for="genero">Gênero: </label>
+            <select name="genero" id="genero"
+                    class="<?= isset($errosCampos['genero']) ? 'input-erro' : '' ?>">
+                <option value="">---Selecione---</option>
+                <option value="S" <?= $genero == "S" ? "selected" : "" ?>>Sertanejo</option>
+                <option value="R" <?= $genero == "R" ? "selected" : "" ?>>Rock</option>
+                <option value="G" <?= $genero == "G" ? "selected" : "" ?>>Gospel</option>
+                <option value="O" <?= $genero == "O" ? "selected" : "" ?>>Outro</option>
+            </select>
+            <?php if (isset($errosCampos['genero'])): ?>
+                <div class="mensagem-erro"><?= $errosCampos['genero'] ?></div>
+            <?php endif; ?>
+        </div>
 
-            <div style="margin-bottom: 10px;">
-                <label for="genero">Gênero: </label>
-                <select name="genero" id="genero">
-                    <option value="">---Selecione---</option>
-                    <option value="S" <?=$genero == "S"?"selected":""?>>Sertanejo</option>
-                    <option value="R"<?=$genero == "R"?"selected":""?>>Rock</option>
-                    <option value="G"<?=$genero == "G"?"selected":""?>>Gospel</option>
-                    <option value="O"<?=$genero == "O"?"selected":""?>>Outro</option>
-                </select>
-            </div>
+        <div>
+            <label for="artista">Artista: </label>
+            <input type="text" name="artista" id="artista"
+                value="<?= $artista ?>"
+                class="<?= isset($errosCampos['artista']) ? 'input-erro' : '' ?>"/>
+            <?php if (isset($errosCampos['artista'])): ?>
+                <div class="mensagem-erro"><?= $errosCampos['artista'] ?></div>
+            <?php endif; ?>
+        </div>
 
-            <div style="margin-bottom: 10px;">
-                <label for="artista">Artista: </label>
-                <input type="text" name="artista" id="artista" value="<?= $artista ?>"/>
-            </div>
+        <div>
+            <label for="linkImagem">Link da Imagem: </label>
+            <input type="text" name="linkImagem" id="linkImagem"
+                value="<?= $linkImagem ?>"
+                class="<?= isset($errosCampos['linkImagem']) ? 'input-erro' : '' ?>"/>
+            <?php if (isset($errosCampos['linkImagem'])): ?>
+                <div class="mensagem-erro"><?= $errosCampos['linkImagem'] ?></div>
+            <?php endif; ?>
+        </div>
 
-            <div style="margin-bottom: 10px;">
-                <label for="linkImagem">Link da Imagem: </label>
-                <input type="text" name="linkImagem" id="linkImagem" value="<?= $linkImagem ?>"/>
-            </div>
+        <div>
+            <label for="corCard">Cor do Card: </label>
+            <select name="corCard" id="corCard"
+                    class="<?= isset($errosCampos['corCard']) ? 'input-erro' : '' ?>">
+                <option value="">---Selecione---</option>
+                <option value="G" <?= $corCard == "G" ? "selected" : "" ?>>Verde</option>
+                <option value="R" <?= $corCard == "R" ? "selected" : "" ?>>Vermelho</option>
+                <option value="Y" <?= $corCard == "Y" ? "selected" : "" ?>>Amarelo</option>
+                <option value="P" <?= $corCard == "P" ? "selected" : "" ?>>Roxo</option>
+                <option value="A" <?= $corCard == "A" ? "selected" : "" ?>>Aleatório</option>
+            </select>
+            <?php if (isset($errosCampos['corCard'])): ?>
+                <div class="mensagem-erro"><?= $errosCampos['corCard'] ?></div>
+            <?php endif; ?>
+        </div>
 
-            <div style="margin-bottom: 10px;">
-                <label for="corCard">Cor do Card: </label>
-                <select name="corCard" id="corCard">
-                    <option value="">---Selecione---</option>
-                    <option value="G" <?=$corCard == "G"?"selected":""?>>Verde</option>
-                    <option value="R"<?=$corCard == "R"?"selected":""?>>Vermelho</option>
-                    <option value="Y"<?=$corCard == "Y"?"selected":""?>>Amarelo</option>
-                    <option value="P"<?=$corCard == "P"?"selected":""?>>Roxo</option>
-                    <option value="A"<?=$corCard == "A"?"selected":""?>>Aleatório</option>
-                </select>
-            </div>
+        <div>
+            <label for="linkMusica">Link da Música: </label>
+            <input type="text" name="linkMusica" id="linkMusica"
+                value="<?= $linkMusica ?>"
+                class="<?= isset($errosCampos['linkMusica']) ? 'input-erro' : '' ?>"/>
+            <?php if (isset($errosCampos['linkMusica'])): ?>
+                <div class="mensagem-erro"><?= $errosCampos['linkMusica'] ?></div>
+            <?php endif; ?>
+        </div>
 
-            <div style="margin-bottom: 10px;">
-                <label for="linkMusica">Link da Musica: </label>
-                <input type="text" name="linkMusica" id="linkMusica" value="<?= $linkMusica ?>"/>
-            </div>
+        <div>
+            <button type="submit">Gravar</button>
+        </div>
+    </form>
 
-            <div style="margin-bottom: 10px;">
-                <button type="submit">Gravar</button>
-            </div>
-        </form>
     
         
         <div id="divErro" style="color: red;">
